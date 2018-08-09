29a559705ff1cc664352bd735f9c21fd948b0922
Haris120 <41987274+Haris120@users.noreply.github.com>
2018-08-09 18:56:58 +0530

    Add files via upload

---
+++ myfirst file.py
@@ -0,0 +1,77 @@
+
+#Variable Declartion
+X=2
+print(X)
+
+y=X*7
+print(y)
+
+X="Hello World! I am"
+print(X)
+
+print(X+'Python')
+
+#Variables in Python
+print(type(2))
+print(type("Hello"))
+print(type(None))
+print(type(int))
+print(type(type(int)))
+
+#Python has two numeric types int and float
+print(3)
+print(3.0)
+print(1+1)
+print(8-1)
+print(10*2)
+print(9/3)
+print(5/2)
+print(7/1.4)
+
+#bool is a subset of int,where True==1 and False==0
+
+print(True)
+print(False)
+print(not True)
+print(True and False)
+print(True or False)
+
+#No char in python
+
+greeting='Hello'
+print(greeting)
+
+group='ayaan'
+print(group)
+
+print(greeting+''+group+'!')
+
+#Indexing in Python
+
+s='ILoveMymom'
+print(s[0])
+print(s[1])
+print(s[2])
+print(s[4])
+print(s[5])
+print(s[6])
+
+#Negative Indexing In Python
+print(s[-1])
+print(s[-2])
+print(s[-3])
+print(s[-4])
+print(s[-5])
+
+#Slicing(Select the particular name or word)
+print(s[1:5:2])
+
+#Reversing a string
+print(s[::-1])
+
+
+
+
+
+
+  
